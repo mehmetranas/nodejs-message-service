b00699de7f19999dcacbce77cc898b91c9259ad4
mehmetranas <mhmtranas@gamil.com>
2017-12-19 22:09:59 +0300

    Test6 for bundle.js request

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('cache-control', 'no-cache');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     next();
--- assets/app/main.ts
+++ assets/app/main.ts
@@ -3,8 +3,6 @@ import './polyfills';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from "./app.module";
-import {enableProdMode} from '@angular/core';
-
-enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
