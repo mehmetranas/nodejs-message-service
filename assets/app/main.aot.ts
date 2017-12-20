aac577158c7eb341c352516ebc2bc53e40b7aa4c
Mehmet <mhmtranas@gmail.com>
2017-12-20 18:55:44 +0300

    delete enableBuild function

--- assets/app/main.aot.ts
+++ assets/app/main.aot.ts
@@ -4,6 +4,6 @@ import { enableProdMode } from "@angular/core";
 
 import { AppModuleNgFactory } from './app.module.ngfactory';
 
-enableProdMode();
+
 
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
